9127f886e2259ad2ddabb3d4e46d5a12d157c642
craigambrose <craig@craigambrose.com>
2017-01-28 10:34:12 +1100

    cleaned up the tests with some helpers

--- test/feed_stage/all_articles_test.exs
+++ test/feed_stage/all_articles_test.exs
@@ -8,36 +8,53 @@ defmodule FeedStage.AllArticlesTest do
   end
 
   test "with no available urls, no events are emmitted" do
-    scraper = Stubr.stub!([])
-    state = %{url_repository: MockRepository, feed_scraper: scraper, buffer: []}
+    state = stub_state()
 
     assert {:noreply, [], _} = AllArticles.handle_demand(10, state)
   end
 
   test "where the first url has sufficient articles to meet demand" do
-    MockRepository.set(["url1"])
-    scraper = Stubr.stub!([get_articles: fn "url1" -> [1,2,3,4] end])
-    state = %{url_repository: MockRepository, feed_scraper: scraper, buffer: []}
+    state = stub_state(%{"url1" => [1,2,3,4]})
 
     assert {:noreply, events, _} = AllArticles.handle_demand(4, state)
     assert Enum.count(events) == 4
   end
 
   test "where the first url has more articles than demanded it saves some for the next call" do
-    MockRepository.set(["url1"])
-    scraper = Stubr.stub!([get_articles: fn "url1" -> [1,2,3,4,5,6,7] end])
-    state = %{url_repository: MockRepository, feed_scraper: scraper, buffer: []}
+    state = stub_state(%{"url1" => [1,2,3,4,5,6,7]})
 
     assert {:noreply, [1,2,3], output_state} = AllArticles.handle_demand(3, state)
     assert [4,5,6,7] == output_state.buffer
   end
 
   test "when there are enough articles in the buffer, use those instead of fetching" do
-    MockRepository.set(["url1"])
-    scraper = Stubr.stub!([get_articles: fn "url1" -> [1,2,3,4,5,6,7] end])
-    state = %{url_repository: MockRepository, feed_scraper: scraper, buffer: [8,9,10,11]}
+    state = stub_state(%{"url1" => [1,2,3,4,5,6,7]}, buffer: [8,9,10,11])
 
     assert {:noreply, [8,9,10], output_state} = AllArticles.handle_demand(3, state)
     assert [11] == output_state.buffer
+  end
+
+  # test "keep fetching feeds until enough articles are available to meet demand"
+  #
+  # test "if you run out of feeds, give up and return what you have"
+  #
+  # test "if there is a problem fetching a url, tell the url repository"
+
+  # --------- HELPERS ------------
+
+  defp stub_scraper(url_mappings) do
+    Stubr.stub!([
+      get_articles: fn url ->
+        url_mappings[url]
+      end
+    ])
+  end
+
+  defp stub_state(url_mappings \\ %{}, other_args \\ %{}) do
+    urls = Map.keys(url_mappings)
+    MockRepository.set(urls)
+    scraper = stub_scraper(url_mappings)
+    result = %{url_repository: MockRepository, feed_scraper: scraper, buffer: []}
+    Map.merge(result, Enum.into(other_args, %{}))
   end
 end
