dacc9163eab527c046b40b8e2a35ba4d6df3dd52
craigambrose <craig@craigambrose.com>
2017-01-27 22:54:34 +1100

    Buffering articles to meet demand

--- lib/feed_stage/all_articles.ex
+++ lib/feed_stage/all_articles.ex
@@ -24,13 +24,45 @@ defmodule FeedStage.AllArticles do
       also can be used to mark a url as checked.
     - url_scraper: A module that has a function to scrape a url.
   """
-  def init({url_repository, _url_scraper}) do
-    state = %{url_repository: url_repository}
+  def init({url_repository, feed_scraper}) do
+    state = %{url_repository: url_repository, feed_scraper: feed_scraper, buffer: []}
     {:producer, state}
   end
 
   def handle_demand(demand, state) when demand > 0 do
-    url = state.url_repository.pop_url()
-    {:noreply, [], state}
+    state = buffer_demanded_articles(demand, state)
+    {articles, state} = retrieve_articles_from_buffer(demand, state)
+
+    {:noreply, articles, state}
   end
+
+  # ----------------------- PRIVATE -----------------------
+
+  defp buffer_demanded_articles(demand, state) do
+    if length(state.buffer) < demand do
+      case buffer_articles(state) do
+        {:ok, state} ->     buffer_demanded_articles(demand, state)
+        {:no_url, state} -> state
+      end
+    else
+      state
+    end
+  end
+
+  defp buffer_articles(state) do
+    url = state.url_repository.pop_url()
+    buffer_articles_from_url(url, state)
+  end
+
+  defp buffer_articles_from_url(url, state) when url == nil, do: {:no_url, state}
+  defp buffer_articles_from_url(url, state) do
+    articles = state.feed_scraper.get_articles(url)
+    {:ok, %{state | buffer: state.buffer ++ articles}}
+  end
+
+  defp retrieve_articles_from_buffer(demand, state) do
+    {demanded, remainder} = Enum.split(state.buffer, demand)
+    {demanded, %{state | buffer: remainder}}
+  end
+
 end
--- test/feed_stage/all_articles_test.exs
+++ test/feed_stage/all_articles_test.exs
@@ -1,30 +1,43 @@
+defmodule FeedStage.AllArticlesTest do
+  use ExUnit.Case
+  alias FeedStage.UrlRepository.MockRepository
+  alias FeedStage.AllArticles
 
+  setup do
+    MockRepository.start_link
+  end
 
+  test "with no available urls, no events are emmitted" do
+    scraper = Stubr.stub!([])
+    state = %{url_repository: MockRepository, feed_scraper: scraper, buffer: []}
 
+    assert {:noreply, [], _} = AllArticles.handle_demand(10, state)
+  end
 
-# defmodule FeedStage.AllArticlesTest do
-#   use ExUnit.Case
-#
-#   test "with no available urls, no events are emmitted" do
-#     state = %{url_repository: MockUrlRepository}
-#     assert {:noreply, [], _} = FeedStage.AllArticles.handle_demand(10, state)
-#   end
-#
-#   test "with one url, returns feed items up to demand number" do
-#     state = %{url_repository: TestUrlRepository}
-#     assert {:noreply, [], _} = FeedStage.AllArticles.handle_demand(10, state)
-#   end
-# end
-#
-#
-# class UrlRepository
-#   attr_accessor :urls
-#
-#   def initialize
-#     @urls = []
-#   end
-#
-#   def pop_url() do
-#     urls.pop
-#   end
-# end
+  test "where the first url has sufficient articles to meet demand" do
+    MockRepository.set(["url1"])
+    scraper = Stubr.stub!([get_articles: fn "url1" -> [1,2,3,4] end])
+    state = %{url_repository: MockRepository, feed_scraper: scraper, buffer: []}
+
+    assert {:noreply, events, _} = AllArticles.handle_demand(4, state)
+    assert Enum.count(events) == 4
+  end
+
+  test "where the first url has more articles than demanded it saves some for the next call" do
+    MockRepository.set(["url1"])
+    scraper = Stubr.stub!([get_articles: fn "url1" -> [1,2,3,4,5,6,7] end])
+    state = %{url_repository: MockRepository, feed_scraper: scraper, buffer: []}
+
+    assert {:noreply, [1,2,3], output_state} = AllArticles.handle_demand(3, state)
+    assert [4,5,6,7] == output_state.buffer
+  end
+
+  test "when there are enough articles in the buffer, use those instead of fetching" do
+    MockRepository.set(["url1"])
+    scraper = Stubr.stub!([get_articles: fn "url1" -> [1,2,3,4,5,6,7] end])
+    state = %{url_repository: MockRepository, feed_scraper: scraper, buffer: [8,9,10,11]}
+
+    assert {:noreply, [8,9,10], output_state} = AllArticles.handle_demand(3, state)
+    assert [11] == output_state.buffer
+  end
+end
